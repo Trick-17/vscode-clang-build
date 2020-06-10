d5dfa0c9186f5128feb3c9cf9c60a812cd026978
G. P. Mueller <gpmueller@outlook.com>
2020-06-10 12:58:58 +0200

    Switched to interactive child process. - clang_build is now called using a new InteractiveProcessHandle class, which allows getting information back directly from clang_build inside an interactive Python shell - target selection now shows the available targets - build type selection works - building works - still a lot of debug log messages that need to be cleaned up and streamlined

--- src/extension.ts
+++ src/extension.ts
@@ -1,113 +1,198 @@
 import * as vscode from 'vscode';
 
-// this method is called when the extension is activated
-export function activate(context: vscode.ExtensionContext) {
+const child_process = require('child_process');
 
+
+class InteractiveProcessHandle {
+    private process: any;
+
+    public outputLog: string;
+    public latestOutput: string;
+
+    private processToPromise(process: any) {
+        return new Promise<string>((resolve, reject) => {
+            console.log("+++ creating promise");
+            process.stdout.removeAllListeners();
+            process.stderr.removeAllListeners();
+            process.stdin.removeAllListeners();
+
+            let lastString: string = '';
+
+            process.stdout.on("data", (data: any) => {
+                data = data.toString().trim();
+                this.update(data);
+                if (data.endsWith('>>>')) {
+                    resolve(lastString);
+                }
+                lastString = data;
+            });
+            process.stderr.on("data", (data: any) => {
+                data = data.toString().trim();
+                this.update(data);
+                if (data.endsWith('>>>')) {
+                    resolve(lastString);
+                }
+                lastString = data;
+            });
+            process.stdin.on("error", () => {
+                console.log("Failure in stdin! ... error");
+                reject();
+            });
+            process.stdin.on("close", () => {
+                console.log("Failure in stdin! ... close");
+                reject();
+            });
+            process.stdin.on("end", () => {
+                console.log("Failure in stdin! ... end");
+                reject();
+            });
+            process.stdin.on("disconnect", () => {
+                console.log("Failure in stdin! ... disconnect");
+                reject();
+            });
+            process.stdout.on("error", () => {
+                console.log("Failure in stdout! ... error");
+                reject();
+            });
+            process.stdout.on("close", () => {
+                console.log("Failure in stdout! ... close");
+                reject();
+            });
+            process.stdout.on("end", () => {
+                console.log("Failure in stdout! ... end");
+                reject();
+            });
+            process.stderr.on("error", () => {
+                console.log("Failure in stderr! ... error");
+                reject();
+            });
+            process.stderr.on("close", () => {
+                console.log("Failure in stderr! ... close");
+                reject();
+            });
+            process.stderr.on("end", () => {
+                console.log("Failure in stderr! ... end");
+                reject();
+            });
+            console.log("+++ done creating promise");
+        });
+    }
+
+    private update(data: any) {
+        this.latestOutput = data;
+        this.outputLog += data + "\n";
+        console.log(`logging from update: "${data}"`);
+    }
+
+    public call(command: string): Promise<string> {
+        console.log(`called: "${command.trim()}"`);
+        let promise = this.processToPromise(this.process);
+        this.process.stdin.write(command);
+        return promise;
+    }
+
+    constructor(call: any, options: any) {
+        this.latestOutput = "";
+        this.outputLog = "";
+
+        this.process = child_process.spawn(call, options, { shell: true });
+        this.process.stdout.setEncoding('utf8');
+        this.process.stderr.setEncoding('utf8');
+    }
+};
+
+
+// This method is called when the extension is activated
+export async function activate(context: vscode.ExtensionContext) {
     const outChannel = vscode.window.createOutputChannel("clang-build");
-    outChannel.appendLine('vscode clang-build extension activated');
+    outChannel.appendLine('activating vscode clang-build extension');
 
-    /////////////////////////////////////////////////////////////////////////////////////////
+    let status_clang_build_type: vscode.StatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+    let status_clang_build_build: vscode.StatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+    let status_clang_build_targets: vscode.StatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
 
     const buildTypeCommandId = 'extension.selectBuildType';
-    let command_selectBuildType = vscode.commands.registerCommand(buildTypeCommandId, () => {
-        vscode.window.showInformationMessage('Selecting build type is not yet implemented!');
-
-        // TODO: read file, if present
-        // TODO: ask user for choice of build type
-        // TODO: automate generation of file contents from dictionary
-        if( vscode.workspace.workspaceFolders )
-        {
-            var folder = vscode.workspace.workspaceFolders[0];
-            var filePath = folder.uri.fsPath + '/.clang-build-cfg.toml';
-            var contents = 'build_type = [default]\ntargets = ["<all>"]';
-            vscode.workspace.fs.writeFile(vscode.Uri.file(filePath), Buffer.from(contents));
-
-            outChannel.show();
-            outChannel.appendLine('Wrote a config file '+vscode.Uri.file(filePath));
-        }
-        else
-        {
-            vscode.window.showInformationMessage('Could not select a build type because you do not have a folder opened');
-        }
-    });
-    context.subscriptions.push(command_selectBuildType);
-
-    /////////////////////////////////////////////////////////////////////////////////////////
-
     const buildCommandId = 'extension.build';
-    let command_build = vscode.commands.registerCommand(buildCommandId, () => {
-        vscode.window.showInformationMessage('Triggered a build!');
+    const targetSelectionCommandId = 'extension.selectTargets';
 
-        // TODO: read config file, if present
-        // TODO: automate generating the right command to run from parameters in the file
-        const cp = require('child_process');
-        if( vscode.workspace.workspaceFolders )
-        {
-            var folder = vscode.workspace.workspaceFolders[0];
-            outChannel.show();
-            outChannel.appendLine('running clang-build command');
-            cp.exec('clang-build -V --debug', {cwd: folder.uri.fsPath}, (err: string, stdout: string, stderr: string) => {
-                outChannel.appendLine(stdout);
-                outChannel.appendLine(stderr);
-                if (err) {
-                    console.log('error: ' + err);
-                    outChannel.appendLine('error: ' + err);
+    // ---------------------------------------------------------------------
+    var clang_build = new InteractiveProcessHandle('python', ['-i']);
+    if (vscode.workspace.workspaceFolders) {
+        var folder = vscode.workspace.workspaceFolders[0];
+
+        await clang_build.call('');
+        await clang_build.call('import sys\n');
+        await clang_build.call('import clang_build\n');
+        await clang_build.call('args = clang_build.clang_build.parse_args(sys.argv[1:])\n');
+        await clang_build.call('environment = clang_build.environment.Environment(args)\n');
+        await clang_build.call('environment.set_working_directory(r"' + folder.uri.fsPath + '")\n');
+        await clang_build.call('print("--- environment created")\n');
+        await clang_build.call('project = clang_build.project.Project.from_directory(r"' + folder.uri.fsPath + '", environment)\n');
+        await clang_build.call('print("--- project created")\n');
+
+        let command_selectBuildType = vscode.commands.registerCommand(buildTypeCommandId, () => {
+            const result = vscode.window.showQuickPick(
+                ['Default', 'Release', 'Debug', 'RelWithDebInfo', 'Coverage'],
+                { placeHolder: 'Build type', }
+            ).then((result) => {
+                if (result) {
+                    clang_build.call(`environment.build_type = clang_build.build_type.BuildType.${result}\n`)
+                        .then(() => {
+                            vscode.window.showInformationMessage(`Set build type to "${result}"`);
+                            status_clang_build_type.text = `clang-build: ${result}`;
+                        });
                 }
             });
-        }
-        else
-        {
-            vscode.window.showInformationMessage('Could not launch build command because you do not have a folder opened');
-        }
-    });
-    context.subscriptions.push(command_build);
+        });
 
-    /////////////////////////////////////////////////////////////////////////////////////////
-
-    const targetSelectionCommandId = 'extension.selectTargets';
-    let command_selectTargets = vscode.commands.registerCommand(targetSelectionCommandId, () => {
-        vscode.window.showInformationMessage('Selecting targets is not yet implemented!');
-
-        // TODO: read file, if present
-        // TODO: ask user for choice of target(s)
-        // TODO: automate generation of file contents from dictionary
-        if( vscode.workspace.workspaceFolders )
-        {
-            var folder = vscode.workspace.workspaceFolders[0];
-            var filePath = folder.uri.fsPath + '/.clang-build-cfg.toml';
-            var contents = 'build_type = [default]\ntargets = ["<all>"]';
-            vscode.workspace.fs.writeFile(vscode.Uri.file(filePath), Buffer.from(contents));
+        let command_build = vscode.commands.registerCommand(buildCommandId, () => {
+            vscode.window.showInformationMessage('Triggered a build!');
 
             outChannel.show();
-            outChannel.appendLine('Wrote a config file '+vscode.Uri.file(filePath));
-        }
-        else
-        {
-            vscode.window.showInformationMessage('Could not select targets because you do not have a folder opened');
-        }
-    });
-    context.subscriptions.push(command_selectTargets);
+            outChannel.appendLine('------ running clang-build');
 
-    /////////////////////////////////////////////////////////////////////////////////////////
+            clang_build.call('project.build(environment.build_all, environment.target_list)\n')
+                .then(() => {
+                    outChannel.appendLine(`Full output of script:\n${clang_build.outputLog}`);
+                    outChannel.appendLine('------ done running clang-build');
+                });
+        });
 
-    let status_clang_build_type: vscode.StatusBarItem;
-    status_clang_build_type = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+        let command_selectTargets = vscode.commands.registerCommand(targetSelectionCommandId, async () => {
+            vscode.window.showInformationMessage('Selecting targets is not yet fully implemented!');
+
+            await clang_build.call('target_list = " ".join([str(target) for target in project._get_targets_to_build(environment.build_all, environment.target_list)])\n');
+            let targets = await clang_build.call('print(target_list)\n');
+            let targets_array = targets.trim().split(" ");
+
+            const result = await vscode.window.showQuickPick(
+                targets_array,
+                { placeHolder: 'target', }
+            );
+            if (result) {
+                vscode.window.showInformationMessage(`Selected target "${result}"`);
+                status_clang_build_targets.text = result;
+            }
+        });
+    }
+    // ---------------------------------------------------------------------
+
     status_clang_build_type.command = buildTypeCommandId;
-    status_clang_build_type.text = `clang-build: release`;
+    status_clang_build_type.text = `clang-build: Default`;
     status_clang_build_type.show();
     context.subscriptions.push(status_clang_build_type);
 
-    let status_clang_build_build = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
     status_clang_build_build.command = buildCommandId;
     status_clang_build_build.text = `build`;
     status_clang_build_build.show();
     context.subscriptions.push(status_clang_build_build);
 
-    let status_clang_build_targets = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
     status_clang_build_targets.command = targetSelectionCommandId;
     status_clang_build_targets.text = `[all]`;
     status_clang_build_targets.show();
     context.subscriptions.push(status_clang_build_targets);
+
+    outChannel.appendLine('vscode clang-build extension activated');
 }
 
 // this method is called when the extension is deactivated
